2f34014e78ca1f3e0537f2f5fbf63d51c01d8540
Tvkhalil456 <tvkhalil456@gmail.com>
2025-06-28 05:00:49 +0200

    Update Minecraft.cpp

--- Minecraft.cpp
+++ Minecraft.cpp
@@ -2,9 +2,7 @@
 #include <gdiplus.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <shellapi.h>
-#include <wingdi.h>
 #include <time.h>
 #pragma comment(lib,"gdiplus.lib")
 #pragma comment(lib,"user32.lib")
@@ -12,50 +10,35 @@
 #pragma comment(lib,"winmm.lib")
 using namespace Gdiplus;
 
-const char* b64 = "iVBORw0KGgoAAAANSUhEUgAAAqQAAAK9CAIAAAB9/emtAAAAAXNSR0IArs4c6QAAIABJREFUeJyEveuS3EiSLua3iACQWUWyL+zemdEeyfReeii9gN5IJjMdk5l0tHN2Z6anL2ySdclMABF+0Q9cMqvIXqU1q4uZQGQg4PDP/fPPg/i//m//[...]";
+// --- Début : Ajout fonction pour charger le PNG depuis la ressource ---
+HBITMAP LoadPNGResource()
+{
+    HRSRC hRes = FindResource(NULL, L"PATAPIMPNG", RT_RCDATA);
+    if (!hRes) return NULL;
+    HGLOBAL hData = LoadResource(NULL, hRes);
+    if (!hData) return NULL;
+    void* pData = LockResource(hData);
+    DWORD size = SizeofResource(NULL, hRes);
 
-int v(char c) {
-    return (c >= 'A' && c <= 'Z') ? c - 'A' :
-           (c >= 'a' && c <= 'z') ? c - 'a' + 26 :
-           (c >= '0' && c <= '9') ? c - '0' + 52 :
-           c == '+' ? 62 :
-           c == '/' ? 63 : -1;
-}
+    HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, size);
+    void* pm = GlobalLock(hg);
+    memcpy(pm, pData, size);
+    GlobalUnlock(hg);
 
-unsigned char* dec(const char* i, int* o) {
-    if (!i) return NULL;
-    int l = (int)strlen(i), j = 0, b = 0, bc = 0;
-    unsigned char* oP = (unsigned char*)malloc(l * 3 / 4 + 1);
-    if (!oP) return NULL;
-    for (int k = 0; k < l; k++) {
-        int val = v(i[k]);
-        if (val >= 0) {
-            b = (b << 6) | val;
-            bc += 6;
-            if (bc >= 8) {
-                bc -= 8;
-                oP[j++] = (b >> bc) & 0xFF;
-            }
-        }
-    }
-    *o = j;
-    return oP;
-}
-
-HBITMAP bS(IStream* p) {
-    Bitmap* b = Bitmap::FromStream(p);
-    if (!b || b->GetLastStatus() != Ok) {
-        delete b;
+    IStream* ps = NULL;
+    if (CreateStreamOnHGlobal(hg, TRUE, &ps) != S_OK) {
+        GlobalFree(hg);
         return NULL;
     }
-    HBITMAP h = NULL;
-    if (b->GetHBITMAP(Color(0, 0, 0, 0), &h) != Ok) {
-        delete b;
-        return NULL;
-    }
+    Bitmap* b = Bitmap::FromStream(ps);
+    HBITMAP hBmp = NULL;
+    if (b && b->GetLastStatus() == Ok)
+        b->GetHBITMAP(Color(0,0,0,0), &hBmp);
     delete b;
-    return h;
+    ps->Release();
+    return hBmp;
 }
+// --- Fin ---
 
 LRESULT CALLBACK WndProc(HWND h, UINT m, WPARAM w, LPARAM l) {
     switch (m) {
@@ -72,7 +55,7 @@ LRESULT CALLBACK WndProc(HWND h, UINT m, WPARAM w, LPARAM l) {
             BITMAP bm;
             GetObject(hb, sizeof(bm), &bm);
             StretchBlt(dc, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
-                mdc, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
+                    mdc, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
             SelectObject(mdc, o);
             DeleteDC(mdc);
         }
@@ -110,6 +93,8 @@ void sF(HBITMAP h) {
     DestroyWindow(hwnd);
 }
 
+// ... (toutes les autres fonctions sont inchangées, tu gardes tout ce qu'il y avait après !)
+
 // 2. Glitch graphique (BitBlt paint)
 void gL() {
     HDC dc = GetDC(NULL);
@@ -126,149 +111,9 @@ void gL() {
     ReleaseDC(NULL, dc);
 }
 
-// 3. Overlay gris semi-transparent
-void bL() {
-    HDC dc = GetDC(NULL);
-    RECT r;
-    GetWindowRect(GetDesktopWindow(), &r);
-    HBRUSH br = CreateSolidBrush(RGB(128, 128, 128));
-    BLENDFUNCTION bl = { AC_SRC_OVER, 0, 100, 0 };
-    HDC mdc = CreateCompatibleDC(dc);
-    HBITMAP bm = CreateCompatibleBitmap(dc, r.right, r.bottom);
-    HBITMAP old = (HBITMAP)SelectObject(mdc, bm);
-    FillRect(mdc, &r, br);
-    AlphaBlend(dc, 0, 0, r.right, r.bottom, mdc, 0, 0, r.right, r.bottom, bl);
-    SelectObject(mdc, old);
-    DeleteObject(bm);
-    DeleteDC(mdc);
-    DeleteObject(br);
-    ReleaseDC(NULL, dc);
-}
+// ... (toutes tes autres fonctions de prank inchangées ici !)
 
-// 4. Spam MessageBox "BRR BRR PATAPIM"
-void eM() {
-    for (int i = 0; i < 35; i++) {
-        MessageBoxW(NULL, L"BRR BRR PATAPIM", L"BRR BRR patapim", MB_ICONERROR | MB_OK | MB_TOPMOST);
-        Sleep(20);
-    }
-}
-
-// 5. Ouverture de notepad en boucle
-void cN() {
-    for (int i = 0; i < 7; i++) {
-        STARTUPINFO si = { 0 };
-        si.cb = sizeof(si);
-        PROCESS_INFORMATION pi;
-        if (CreateProcessW(L"C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
-            CloseHandle(pi.hProcess);
-            CloseHandle(pi.hThread);
-        }
-        Sleep(60);
-    }
-}
-
-// 6. Glitch graphique (BitBlt paint 2)
-void gL2() {
-    HDC dc = GetDC(NULL);
-    RECT r;
-    GetWindowRect(GetDesktopWindow(), &r);
-    for (int i = 0; i < 35; i++) {
-        int x = rand() % r.right;
-        int y = rand() % r.bottom;
-        int w = 60 + rand() % 120;
-        int h = 32 + rand() % 80;
-        BitBlt(dc, x, y, w, h, dc, x + 5, y + 3, SRCPAINT);
-        Sleep(13);
-    }
-    ReleaseDC(NULL, dc);
-}
-
-// 7. Spam MessageBox "BRR BRR TRALALA ?"
-void sM() {
-    for (int i = 0; i < 35; i++) {
-        UINT t = (i % 2 == 0) ? MB_ICONERROR | MB_OK : MB_ICONQUESTION | MB_YESNO | MB_TOPMOST;
-        int r = MessageBoxW(NULL, L"BRR BRR TRALALA ?", L"Attention", t);
-        Sleep(10);
-    }
-}
-
-// 8. Beep sons
-void pS() {
-    for (int i = 0; i < 10; i++) {
-        Beep(1000 + (i * 200), 200);
-        Sleep(100);
-    }
-}
-
-// 9. Mode négatif clignotant
-void effetNegatif() {
-    HDC hdc = GetDC(NULL);
-    int sx = GetSystemMetrics(SM_CXSCREEN);
-    int sy = GetSystemMetrics(SM_CYSCREEN);
-    for (int k = 0; k < 7; ++k) {
-        BitBlt(hdc, 0, 0, sx, sy, hdc, 0, 0, NOTSRCCOPY);
-        Sleep(800);
-        BitBlt(hdc, 0, 0, sx, sy, hdc, 0, 0, NOTSRCCOPY);
-        Sleep(800);
-    }
-    ReleaseDC(NULL, hdc);
-}
-
-// 10. Ouvre la calculatrice
-void effetCalc() {
-    for (int i = 0; i < 3; i++) {
-        ShellExecuteW(0, 0, L"calc.exe", 0, 0, SW_SHOW);
-        Sleep(300);
-    }
-}
-
-// 11. Inversion horizontale écran
-void effetMirror() {
-    HDC hdc = GetDC(NULL);
-    int sx = GetSystemMetrics(SM_CXSCREEN), sy = GetSystemMetrics(SM_CYSCREEN);
-    StretchBlt(hdc, sx, 0, -sx, sy, hdc, 0, 0, sx, sy, SRCCOPY);
-    ReleaseDC(NULL, hdc);
-}
-
-// 12. Déplacement rapide souris
-void effetSouris() {
-    POINT pt;
-    for (int i = 0; i < 20; i++) {
-        pt.x = rand() % GetSystemMetrics(SM_CXSCREEN);
-        pt.y = rand() % GetSystemMetrics(SM_CYSCREEN);
-        SetCursorPos(pt.x, pt.y);
-        Sleep(70);
-    }
-}
-
-// 13. Affichage smileys partout
-void effetSmileys() {
-    HDC dc = GetDC(NULL);
-    for (int i = 0; i < 24; i++) {
-        TextOutW(dc, rand() % GetSystemMetrics(SM_CXSCREEN), rand() % GetSystemMetrics(SM_CYSCREEN), L":)", 2);
-        Sleep(25);
-    }
-    ReleaseDC(NULL, dc);
-}
-
-// 14. Superposition grille rouge
-void effetGrille() {
-    HDC dc = GetDC(NULL);
-    int sx = GetSystemMetrics(SM_CXSCREEN), sy = GetSystemMetrics(SM_CYSCREEN);
-    HPEN pen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
-    HGDIOBJ old = SelectObject(dc, pen);
-    for (int x = 0; x < sx; x += 70) {
-        MoveToEx(dc, x, 0, NULL); LineTo(dc, x, sy);
-    }
-    for (int y = 0; y < sy; y += 70) {
-        MoveToEx(dc, 0, y, NULL); LineTo(dc, sx, y);
-    }
-    SelectObject(dc, old);
-    DeleteObject(pen);
-    ReleaseDC(NULL, dc);
-}
-
-// 15. Thread : Error icons tant que la souris bouge
+// Thread icons d'erreur souris
 DWORD WINAPI mouseErrorIconsThread(LPVOID) {
     POINT oldPos, curPos;
     GetCursorPos(&oldPos);
@@ -286,64 +131,42 @@ DWORD WINAPI mouseErrorIconsThread(LPVOID) {
     return 0;
 }
 
-int WINAPI wWinMain(HINSTANCE h, HINSTANCE p, PWSTR c, int n) {
+int WINAPI wWinMain(HINSTANCE h, HINSTANCE, PWSTR, int)
+{
     srand((unsigned)time(NULL));
-    ULONG_PTR t;
-    GdiplusStartupInput i = { sizeof(GdiplusStartupInput) };
-    if (GdiplusStartup(&t, &i, NULL) != Ok) return -1;
+    ULONG_PTR gdipToken;
+    GdiplusStartupInput gdipSI = { sizeof(GdiplusStartupInput) };
+    if (GdiplusStartup(&gdipToken, &gdipSI, NULL) != Ok) return -1;
+
     HANDLE th = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)pS, NULL, 0, NULL);
     CloseHandle(th);
 
     // Lance le thread pour affichage d'icônes d'erreur sur mouvement souris
     CreateThread(NULL, 0, mouseErrorIconsThread, NULL, 0, NULL);
 
-    int sz = 0;
-    unsigned char* d = dec(b64, &sz);
-    if (!d) {
-        GdiplusShutdown(t);
-        return -1;
-    }
-    HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, sz);
-    if (!hg) {
-        free(d);
-        GdiplusShutdown(t);
-        return -1;
-    }
-    void* pm = GlobalLock(hg);
-    memcpy(pm, d, sz);
-    GlobalUnlock(hg);
-    IStream* ps = NULL;
-    if (CreateStreamOnHGlobal(hg, TRUE, &ps) != S_OK) {
-        GlobalFree(hg);
-        free(d);
-        GdiplusShutdown(t);
-        return -1;
-    }
-    HBITMAP hb = bS(ps);
-    ps->Release();
-    free(d);
+    HBITMAP hb = LoadPNGResource();
     if (!hb) {
-        GdiplusShutdown(t);
+        GdiplusShutdown(gdipToken);
         return -1;
     }
 
-    // Enchainement des pranks
-    sF(hb);            // 1
-    gL();              // 2
-    bL();              // 3
-    eM();              // 4
-    cN();              // 5
-    gL2();             // 6
-    sM();              // 7
+    // Enchainement des pranks (comme avant)
+    sF(hb);
+    gL();
+    bL();
+    eM();
+    cN();
+    gL2();
+    sM();
     Sleep(100);
-    effetNegatif();    // 9
-    effetCalc();       // 10
-    effetMirror();     // 11
-    effetSouris();     // 12
-    effetSmileys();    // 13
-    effetGrille();     // 14
+    effetNegatif();
+    effetCalc();
+    effetMirror();
+    effetSouris();
+    effetSmileys();
+    effetGrille();
 
     DeleteObject(hb);
-    GdiplusShutdown(t);
+    GdiplusShutdown(gdipToken);
     return 0;
 }
